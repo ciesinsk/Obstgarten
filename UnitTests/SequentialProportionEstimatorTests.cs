4a4770fd9ed10eaca2efe2d70bbfc4724a0b0890
ciesinsk <32510276+ciesinsk@users.noreply.github.com>
2026-08-21 17:48:08 +0200

    test: cover finite precision stopping calculation

--- UnitTests/SequentialProportionEstimatorTests.cs
+++ UnitTests/SequentialProportionEstimatorTests.cs
@@ -6,12 +6,30 @@ namespace UnitTests
     public class SequentialProportionEstimatorTests
     {
         [TestMethod]
+        public void PrecisionForOneDecimalPercentageIsHalfOfLastDisplayedUnit()
+        {
+            var precision = SequentialProportionEstimator
+                .PrecisionForPercentageDecimalPlaces(1);
+
+            Assert.AreEqual(0.0005, precision, 1e-12);
+        }
+
+        [TestMethod]
+        public void RequiredTrialsForNinetyPercentAndOneDecimalPercentageIsFinite()
+        {
+            var required = SequentialProportionEstimator.GetRequiredTrials(
+                absoluteError: 0.0005,
+                confidenceLevel: 0.90);
+
+            Assert.AreEqual(5_991_465L, required);
+        }
+
+        [TestMethod]
         public void ConfidenceIntervalContainsEstimate()
         {
             var interval = SequentialProportionEstimator.GetConfidenceInterval(
                 successes: 600,
                 trials: 1000,
-                inspectionNumber: 1,
                 confidenceLevel: 0.90);
 
             Assert.IsTrue(interval.Lower <= interval.Estimate);
@@ -20,46 +38,20 @@ namespace UnitTests
         }
 
         [TestMethod]
-        public void ConfidenceIntervalShrinksWithMoreTrialsAtSameInspection()
+        public void ConfidenceIntervalShrinksWithMoreTrials()
         {
             var smallerSample = SequentialProportionEstimator.GetConfidenceInterval(
                 successes: 600,
                 trials: 1000,
-                inspectionNumber: 1,
                 confidenceLevel: 0.90);
 
             var largerSample = SequentialProportionEstimator.GetConfidenceInterval(
                 successes: 6000,
                 trials: 10000,
-                inspectionNumber: 1,
                 confidenceLevel: 0.90);
 
             Assert.IsTrue(largerSample.Upper - largerSample.Lower <
                           smallerSample.Upper - smallerSample.Lower);
-        }
-
-        [TestMethod]
-        public void RoundedPercentageIsStableWhenWholeIntervalIsInOneRoundingCell()
-        {
-            var interval = new SequentialProportionEstimator.ConfidenceInterval(
-                Estimate: 0.6342,
-                Lower: 0.6338,
-                Upper: 0.6344);
-
-            Assert.IsTrue(
-                SequentialProportionEstimator.IsRoundedPercentageStable(interval, decimalPlaces: 1));
-        }
-
-        [TestMethod]
-        public void RoundedPercentageIsNotStableWhenIntervalCrossesRoundingBoundary()
-        {
-            var interval = new SequentialProportionEstimator.ConfidenceInterval(
-                Estimate: 0.6342,
-                Lower: 0.6338,
-                Upper: 0.6352);
-
-            Assert.IsFalse(
-                SequentialProportionEstimator.IsRoundedPercentageStable(interval, decimalPlaces: 1));
         }
     }
 }
